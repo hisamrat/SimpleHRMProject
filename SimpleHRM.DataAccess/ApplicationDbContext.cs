b1318508fd24d5e3a4a4ff0e1df6a2cee07f9692
Rakibul Islam Samrat <49041067+hisamrat@users.noreply.github.com>
2023-01-10 01:29:14 +0600

    Packages and Connections

--- SimpleHRM.DataAccess/ApplicationDbContext.cs
+++ SimpleHRM.DataAccess/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace SimpleHRM.DataAccess
 {
-   public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
     }
 }
